34ec55c7b6b663d1cadfac2d2ca8d7579bbfdb16
YeahYuan <39723402+YeahYuan@users.noreply.github.com>
2018-06-27 19:34:47 +0800

    @袁二

--- Test.java
+++ Test.java
@@ -1,95 +1,45 @@
-package cn.itcast.day23.io.test;
+package cn.itcast.day24.io.encode;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.FilenameFilter;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.UnsupportedEncodingException;
 
-/*
- * 综合练习:
- * 获取指定目录下,指定扩展名的文件(包含子目录),将这些文件的绝对路径写入到一个文本文件中
- * 简单说,就是建立一个指定扩展名的文件列表
- * 
- * 思路:
- * 1.深度遍历
- * 2.遍历时过滤,将符合的内容存储在容器中
- * 3.对容器遍历将绝对路径写入文件
- */
 public class Test {
-
+	/*
+	 * 需求:定义一个方法,按照最大的字节数来取子串 说明: java中字符串"abcd"和"ab你好"的长度一样,都是四个字符
+	 * 但对应的字节数不同,因为一个汉字占两个字节 例:对于"ab你好" 取三个字节应舍弃"你"的半个,为"ab"; 取四个字节则不舍弃,为"ab你";
+	 * 取五个字节舍弃半个"好",仍为"ab你".
+	 */
 	public static void main(String[] args) throws IOException {
-		File dir = new File("C:\\Users\\lll\\Desktop\\1.毕向东Java视频(史上最适合初学者入门的Java基础视频)\\1.code 袁二练习");
-		String filterName = ".class";
-		FilenameFilter filter = getFilter(filterName);
+		String str = "ab你好c哈";
 		
-		ArrayList<File> list = new ArrayList<File>();
-		getFiles(dir, filter, list);
-		
-		File destFile = new File(dir, "javalist.txt");
-		writerToFile(list, destFile);
-	}
-
-	public static void writerToFile(ArrayList<File> list, File destFile) throws IOException {
-		BufferedWriter bufw = null;
-		try {
-			bufw = new BufferedWriter(new FileWriter(destFile));
-			for (File file : list) {
-				bufw.write(file.getAbsolutePath());
-				bufw.newLine();
-				bufw.flush();
-			}
-		} catch (IOException e) {
-			throw new RuntimeException("写入失败");
-		} finally {
-			if(bufw!=null) {
-				try {
-					bufw.close();
-				} catch (IOException e) {
-					throw new RuntimeException("关闭失败");
-				}
-			}
-		}
-	}
-/**
- * 对指定目录中的内容进行深度遍历,并按照指定过滤器进行过滤
- * 将过滤后的内容存储在指定容器list中.
- * @param dir
- * @param filter
- * @param list
- * @throws IOException
- */
-	public static void getFiles(File dir, FilenameFilter filter, ArrayList<File> list) throws IOException {
-		File[] files = dir.listFiles();
-		for (File file : files) {
-			if (file.isDirectory()) {
-				getFiles(file, filter, list);
-			} else if (filter.accept(dir, file.getName()))
-				list.add(file);
+		for(int len=1; len<=str.getBytes("GBK").length; len++) {
+			String s1 = cutStringByByte(str,len,"GBK");
+			System.out.println("截取"+len+"个字节结果是:"+s1);
 		}
 	}
 
-	private static FilenameFilter getFilter(String filterName) {
-		FilenameFilter filter = new FilenameFilterByName(filterName);
-		return filter;
+	public static String cutStringByByte(String str,int len,String charseName) throws IOException {
+		byte[] buf = str.getBytes(charseName);
+		int count = 0;
+		for(int x=len-1; x>=0; x--) {
+			if(buf[x]<0) {
+				count++;
+			}
+			else
+				break;
+		}
+		if(count%2==0) {
+			return new String(buf,0,len,charseName);
+		}
+		else
+			return new String(buf,0,len-1,charseName);
 	}
-
 }
 
-class FilenameFilterByName implements FilenameFilter {
-	String filterName = null;
 
-	FilenameFilterByName(String filterName) {
-		this.filterName = filterName;
-	}
 
-	@Override
-	public boolean accept(File dir, String name) {
-		return name.endsWith(filterName);
-	}
 
-}
+
 
 
 
